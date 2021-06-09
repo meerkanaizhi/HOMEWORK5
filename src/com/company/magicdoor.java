eb3884d0addc2fe3e68bc43cf7e1cc7f3d94d161
meerkanaizhi <85063110+meerkanaizhi@users.noreply.github.com>
2021-06-10 00:45:50 +0600

    Update magicdoor.java

--- src/com/company/magicdoor.java
+++ src/com/company/magicdoor.java
@@ -1,5 +1,20 @@
+package com.company;
+
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class magicdoor {
+    public static void main (String[] args){
+
+        String[] names = new String[4];
+        names[0] = "Iron man";
+        names[1] = "Big";
+        names[2] = "Meri";
+        names[3] = "Cat";
+
+        String[] namesNew = Arrays.copyOf(names, names.length + 1 );
+        namesNew [4] = "Lili";
+        System.out.println(namesNew.length);
+        System.out.println(namesNew[0]);
+
+    }
 }
